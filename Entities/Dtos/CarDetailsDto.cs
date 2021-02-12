9252e2c70d2db8735fb490fd9ab9b3cf5c758fba
Barış KARAKAYA <181805054@ofis365.adu.edu.tr>
2021-02-12 21:20:52 +0300

    Dtos Eklendi, IEntity Çıkarıldı. Araba Kiralama Menüsü için Özel tablo nesnesi oluşturuldu.

--- Entities/Dtos/CarDetailsDto.cs
+++ Entities/Dtos/CarDetailsDto.cs
@@ -3,16 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Concrete
+namespace Entities.Dtos
 {
-    public class Car : IEntity
+    public class CarDetailsDto : IDto
     {
         public int CarID { get; set; }
-        public int BrandID { get; set; }
-        public int ColorID { get; set; }
         public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
         public string ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Descriptions { get; set; }
+
     }
 }
